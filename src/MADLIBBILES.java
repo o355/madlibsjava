2cdf2f221f129e0a7024158ec0337f928df1bf11
o355 <owen@o355.us>
2015-06-03 22:36:38 -0400

    Main file

---
+++ src/MADLIBBILES.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+
+public class MADLIBBIES {
+
+	public static void main(String[] args) throws InterruptedException {
+		Scanner scan=new Scanner (System.in);
+		
+		//declare variables
+		String object;
+		String place;
+		String time;
+		String verbEndingInE;
+		String feeling;
+		String time2;
+		String email;
+		
+		System.out.println("Loading - 0% (6.3 seconds remain)");
+		Thread.sleep(4000l);
+		System.out.println("Loading - 22% (2.3 seconds remain)");
+		Thread.sleep(950l);
+		System.out.println("Loading - 43% (1.3 seconds remain)");
+		Thread.sleep(766l);
+		System.out.println("Loading - 100% (0.6 seconds remain)");
+		Thread.sleep(566l);
+		System.out.println("");
+		System.out.println("Mad Libs - Version 1.3 beta (Released on July 9, 2014, at 11:17am. Created by o355.");
+		System.out.println("");
+		
+		
+		//except and ask for input
+		System.out.println("");
+		System.out.println("Spaces are now allowed!");
+	    System.out.println("New in 1.1 - Bug fixes!");
+	    System.out.println("New in 1.2 - Allowed spaces, added loading!");
+	    System.out.println("New in 1.3 - Added more to the Mad Lib!");
+		
+		System.out.println("");
+		System.out.println("Give me an object.");
+		object=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Inputting Data, please wait. (1.5 seconds remain)");
+		Thread.sleep(1500l);
+		
+		System.out.println("");
+		System.out.println("Give me a place!");
+		place=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Inputting Data, please wait. (1.5 seconds remain)");
+		Thread.sleep(1500l);
+		
+		System.out.println("");
+		System.out.println("Give me a time");
+		time=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Inputting Data, please wait. (2.2 seconds remain)");
+		Thread.sleep(2200l);
+		
+		System.out.println("");
+		System.out.println("Give me a verb ending in ed (Stopped, etc.)");
+		verbEndingInE=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Inputting Data, please wait. (1.7 seconds remain)");
+		Thread.sleep(1700l);
+		
+		System.out.println("");
+		System.out.println("Give me another time!");
+		time2=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Inputting Data, please wait. (1.6 seconds remain)");
+		Thread.sleep(1600l);
+		
+		System.out.println("");
+		System.out.println("Give me a message you would find in an email!");
+		email=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Inputting Data, please wait. (6.2 seconds remain)");
+		Thread.sleep(6200l);
+		
+		System.out.println("");
+		System.out.println("Give me a feeling!");
+		feeling=scan.nextLine();
+		
+		System.out.println("");
+		System.out.println("Making your mad lib! - 0% (11.5 seconds remain)");
+		Thread.sleep(7523l);
+		System.out.println("Making your mad lib! - 39% (4 seconds remain)");
+		Thread.sleep(665l);
+		System.out.println("Making your mad lib! - 58% (3.4 seconds remain)");
+		Thread.sleep(1823l);
+		System.out.println("Making your mad lib! - 100% (1.5 seconds remain)");
+		Thread.sleep(1543l);
+		System.out.println("");
+		
+		
+		
+		
+		
+		System.out.println("");
+		System.out.println("Today, I went with my pet " + object + " and went to " + place + ". Once it was " + time + ", we went to " + place + ". All of a sudden, we got " + verbEndingInE + " out. Oh well, I felt " + feeling + " afterwards! I should probably go to bed soon, maybe around " + time2 + ". The next morning, I recevied an email saying... " + email + ". Yikes.");
+		System.out.println("");
+		System.out.println("Thanks for using Mad Libs by o355! I will be adding more to the project very soon!");
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
